2c83c8ef82049ab60ca3afd82a8431d106e089c9
Sarah Manicke <sm.wit2022@startsteps.de>
2023-01-08 19:31:42 +0100

    create BookList Component

---
+++ client/src/components/BookList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const BookList = (props: Props) => {
+  return (
+    <div>
+        <ul id="book-list">
+            <li>Book Name</li>
+        </ul>
+    </div>
+  )
+}
+
+export default BookList
