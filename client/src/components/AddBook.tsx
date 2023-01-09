c94d488e013193a2d2f41ce080ec761bee646935
Sarah Manicke <sm.wit2022@startsteps.de>
2023-01-09 09:18:02 +0100

    Add Book Component

---
+++ client/src/components/AddBook.tsx
@@ -0,0 +1,64 @@
+import React, { Key } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+type Props = {}
+
+interface AuthorData {
+    name: String,
+    id: Key
+}
+
+const getAuthorsQuery = gql`
+    {
+        authors{
+            name
+            id
+        }
+    }
+`
+
+const AddBook = (props: Props) => {
+
+    const { loading, error, data } = useQuery(getAuthorsQuery);
+
+    const displayAuthors = () =>{
+        if (loading) return <option disabled>Loading Authors...</option>;
+        if (error) return <option disabled>Error Loading Authors</option>;
+        if(data){
+            const {authors} = data;
+            return authors.map((author: AuthorData, index: Key) => {
+                return (<option key={index} value={author.id}> {author.name} </option>);
+            })
+        }
+    }
+
+  return (
+    <div>
+        <form id="add-book">
+        <div className="field">
+          <label>Book name:</label>
+          <input type="text"/>
+        </div>
+        
+        <div className="field">
+          <label>Genre:</label>
+          <input type="text"/>
+        </div>
+
+
+        <div className="field">
+          <label>Author:</label>
+          <select>
+            <option>Select Author</option>
+            {displayAuthors()}
+          </select>
+        </div>
+
+
+        <button>+</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddBook
